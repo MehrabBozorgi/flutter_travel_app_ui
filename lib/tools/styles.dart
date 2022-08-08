76e03dda673eea7229a68227204dd733e51fe97f
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-08-08 12:15:39 +0430

    travel_app_ui

---
+++ lib/tools/styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+const textStyle1=TextStyle(fontSize: 32,color: Colors.black,fontWeight: FontWeight.bold,);
+const textStyle2=TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold,);
+//
+const textStyle3=TextStyle(fontSize: 18,color: Color(0xFF757575),fontWeight: FontWeight.bold,);
+const textStyle4=TextStyle(fontSize: 16,color: Colors.grey,fontWeight: FontWeight.bold,);
+//
+const textStyle5=TextStyle(fontSize: 18,color: Colors.white,fontWeight: FontWeight.bold,);
+const textStyle6=TextStyle(fontSize: 16,color: Colors.white,fontWeight: FontWeight.normal,);
+const textStyle7=TextStyle(fontSize: 14,color: Colors.white,fontWeight: FontWeight.normal,);
+//
+const textStyle8=TextStyle(fontSize: 18,color: color,fontWeight: FontWeight.bold,);
+const textStyle9=TextStyle(fontSize: 16,color: primaryColor,fontWeight: FontWeight.bold,);
+const textStyle10=TextStyle(fontSize: 25,color: Colors.white,fontWeight: FontWeight.bold,);
+
+
+
