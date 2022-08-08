a4ea49083c3b3bc02489ddbc4e5817a85768e80d
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-08-08 12:14:28 +0430

    travel_app_ui

---
+++ lib/tools/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const backGroundColor1=Color(0xff25242A);
+const backGroundColor2=Color(0xff24242E);
+const navColor=Color(0xff313342);
+const primaryColor=Color(0xff3C6FE8);
+const color=Color(0xffC45453);
